87bb823a5f47b5461c337ba4aa40538506b2d3a3
Devwrat Nevewani <devwratneve@gmail.com>
2020-05-14 00:46:43 +0530

    trying to remove error

--- myprojectphp.php
+++ myprojectphp.php
@@ -1,6 +1,3 @@
-<body style="background-color:powderblue;">
-        <?php
-            echo "<p>Hello World!</p>";
-	    echo "<p>I am devwrat</p>";
-        ?>
-</body>
+<?php
+            print "<p>Hello World!</p>";
+?>
