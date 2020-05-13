886cdc261ec0f5415bb239f7274adb4494406cc1
Devwrat Nevewani <devwratneve@gmail.com>
2020-05-13 17:17:59 +0530

    like python prog

--- myprojectphp.php
+++ myprojectphp.php
@@ -1,5 +1,6 @@
 <?php
 print "Hello World!<br>";
+print "I know python programming a lot...<br>";
 print "my name is devwrat!!<br>";
 print "I am in my last year of engineering..<br>";
 print " Thank you everyone :) ..<br>we all will meet soon...<br>";
