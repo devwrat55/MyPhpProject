211f64e29195a7caaf047ec4de0002e0e86ac7ed
Devwrat Nevewani <devwratneve@gmail.com>
2020-05-13 16:29:49 +0530

    hello world made capital

--- myprojectphp.php
+++ myprojectphp.php
@@ -1,5 +1,5 @@
 <?php
-print "hello world!<br>";
+print "Hello World!<br>";
 printtt "my name is devwrat!!<br>";
 print "I am in my last year of engineering..<br>";
 print " Thank you everyone :) ..<br>we all will meet soon...<br>";
