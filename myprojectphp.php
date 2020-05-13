98632a49a45eb1d91a11494793b00db5728eba55
Devwrat Nevewani <devwratneve@gmail.com>
2020-05-14 00:40:36 +0530

    tryed to clear error

--- myprojectphp.php
+++ myprojectphp.php
@@ -1,11 +1,6 @@
-<html>
-    <head>
-    <title>My first PHP Website</title>
-    </head>
-    <body style="background-color:powderblue;">
+<body style="background-color:powderblue;">
         <?php
             echo "<p>Hello World!</p>";
 	    echo "<p>I am devwrat</p>";
         ?>
-    </body>
-</html>
+</body>
