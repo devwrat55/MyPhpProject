2417e9a194fbb2a3b55d7da0785d15d862b0d0f2
Devwrat Nevewani <devwratneve@gmail.com>
2020-05-13 17:29:26 +0530

    great job

--- myprojectphp.php
+++ myprojectphp.php
@@ -2,6 +2,6 @@
 print "Hello World!<br>";
 print "I know python programming a lot...<br>";
 print "my name is devwrat!!<br>";
-print "I am doing engineering..<br>";
+print "I am doing great job..<br>";
 print " Thank you everyone :) ..<br>we all will meet soon...<br>";
 ?>
